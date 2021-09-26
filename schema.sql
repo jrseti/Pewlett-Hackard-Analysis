1fc62a6f16c1fef7b0d4a0726f500240bba6623b
Jon Richards <jrseti@gmail.com>
2021-09-26 10:55:28 -0700

    Modified to match module

--- schema.sql
+++ schema.sql
@@ -1,85 +1,201 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/BUqsQc
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+--
+-- PostgreSQL database dump
+--
 
+-- Dumped from database version 13.4
+-- Dumped by pg_dump version 13.4
 
-CREATE TABLE "Departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
+-- Started on 2021-09-26 10:54:28 PDT
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 200 (class 1259 OID 16385)
+-- Name: departments; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.departments (
+    dept_no character varying(4) NOT NULL,
+    dept_name character varying(40) NOT NULL
 );
 
-CREATE TABLE "Employees" (
-    "emp_no" int   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "gender" varchar   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
-     )
+
+ALTER TABLE public.departments OWNER TO postgres;
+
+--
+-- TOC entry 205 (class 1259 OID 16451)
+-- Name: dept_emp; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.dept_emp (
+    dept_no character varying NOT NULL,
+    emp_no integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-CREATE TABLE "Managers" (
-    "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    CONSTRAINT "pk_Managers" PRIMARY KEY (
-        "dept_no"
-     )
+
+ALTER TABLE public.dept_emp OWNER TO postgres;
+
+--
+-- TOC entry 202 (class 1259 OID 16400)
+-- Name: dept_manager; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.dept_manager (
+    dept_no character varying(4) NOT NULL,
+    emp_no integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-CREATE TABLE "Dept_Emp" (
-    "dept_no" varchar   NOT NULL,
-    "emp_no" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
+
+ALTER TABLE public.dept_manager OWNER TO postgres;
+
+--
+-- TOC entry 201 (class 1259 OID 16392)
+-- Name: employees; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employees (
+    emp_no integer NOT NULL,
+    birth_date date NOT NULL,
+    first_name character varying NOT NULL,
+    last_name character varying NOT NULL,
+    gender character varying NOT NULL,
+    hire_date date NOT NULL
 );
 
-CREATE TABLE "Titles" (
-    "emp_no" int   NOT NULL,
-    -- Adds pk restraint to title column
-    "title" varchar   NOT NULL,
-    -- Adds pk restraint to from_date column
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "emp_no","title","from_date"
-     )
+
+ALTER TABLE public.employees OWNER TO postgres;
+
+--
+-- TOC entry 204 (class 1259 OID 16446)
+-- Name: salaries; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.salaries (
+    emp_no integer NOT NULL,
+    salary integer NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-CREATE TABLE "Salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "emp_no"
-     )
+
+ALTER TABLE public.salaries OWNER TO postgres;
+
+--
+-- TOC entry 203 (class 1259 OID 16433)
+-- Name: titles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.titles (
+    emp_no integer NOT NULL,
+    title character varying NOT NULL,
+    from_date date NOT NULL,
+    to_date date NOT NULL
 );
 
-ALTER TABLE "Managers" ADD CONSTRAINT "fk_Managers_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
 
-ALTER TABLE "Managers" ADD CONSTRAINT "fk_Managers_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE public.titles OWNER TO postgres;
 
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+--
+-- TOC entry 3137 (class 2606 OID 16391)
+-- Name: departments departments_dept_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
 
-ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE ONLY public.departments
+    ADD CONSTRAINT departments_dept_name_key UNIQUE (dept_name);
 
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+--
+-- TOC entry 3139 (class 2606 OID 16389)
+-- Name: departments departments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.departments
+    ADD CONSTRAINT departments_pkey PRIMARY KEY (dept_no);
+
+
+--
+-- TOC entry 3143 (class 2606 OID 16404)
+-- Name: dept_manager dept_manager_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_manager
+    ADD CONSTRAINT dept_manager_pkey PRIMARY KEY (emp_no, dept_no);
+
+
+--
+-- TOC entry 3141 (class 2606 OID 16399)
+-- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (emp_no);
+
+
+--
+-- TOC entry 3147 (class 2606 OID 16450)
+-- Name: salaries pk_Salaries; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.salaries
+    ADD CONSTRAINT "pk_Salaries" PRIMARY KEY (emp_no);
+
+
+--
+-- TOC entry 3145 (class 2606 OID 16440)
+-- Name: titles pk_Titles; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.titles
+    ADD CONSTRAINT "pk_Titles" PRIMARY KEY (emp_no, title, from_date);
+
+
+--
+-- TOC entry 3149 (class 2606 OID 16458)
+-- Name: dept_emp pk_dept_emp; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_emp
+    ADD CONSTRAINT pk_dept_emp PRIMARY KEY (dept_no, emp_no);
+
+
+--
+-- TOC entry 3151 (class 2606 OID 16410)
+-- Name: dept_manager dept_manager_dept_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_manager
+    ADD CONSTRAINT dept_manager_dept_no_fkey FOREIGN KEY (dept_no) REFERENCES public.departments(dept_no);
+
+
+--
+-- TOC entry 3150 (class 2606 OID 16405)
+-- Name: dept_manager dept_manager_emp_no_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dept_manager
+    ADD CONSTRAINT dept_manager_emp_no_fkey FOREIGN KEY (emp_no) REFERENCES public.employees(emp_no);
+
+
+-- Completed on 2021-09-26 10:54:28 PDT
+
+--
+-- PostgreSQL database dump complete
+--
 
